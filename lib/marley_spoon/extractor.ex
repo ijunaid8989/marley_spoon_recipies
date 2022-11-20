702c9d5ee231e797da1a164f5b48dbc658cf8521
Junaid Farooq <fjunaid252@gmail.com>
2022-11-20 22:21:11 +0500

    Added Extractor for pulling entities from Contentful.

---
+++ lib/marley_spoon/extractor.ex
@@ -0,0 +1,34 @@
+defmodule MarleySpoon.Extractor do
+  @moduledoc """
+  Request and Extract recipes, tags and chefs for MarleySpoon Contentful
+  """
+
+  use GenServer
+  alias MarleySpoon.Gordan
+
+  require Logger
+
+  @interval 10_000
+  @entities_to_extract [:tag, :chef, :recipe]
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(_) do
+    :timer.send_interval(@interval, :tick)
+
+    {:ok, 0}
+  end
+
+  def handle_info(:tick, state) do
+    if Application.get_env(:marley_spoon, __MODULE__)[:enabled], do: work()
+
+    {:noreply, state}
+  end
+
+  defp work do
+    {:ok, count} = Gordan.mixing(@entities_to_extract)
+    Logger.debug("#{count} entities updated.")
+  end
+end
