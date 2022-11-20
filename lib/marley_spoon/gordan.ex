f3ba8a285dac64d78cb9620ed81f142fd0305abf
Junaid Farooq <fjunaid252@gmail.com>
2022-11-20 17:20:21 +0500

    Added Gordan for mixing all entities.

---
+++ lib/marley_spoon/gordan.ex
@@ -0,0 +1,24 @@
+defmodule MarleySpoon.Gordan do
+  require Logger
+  import MarleySpoon.Request.Contentful, only: [list_entities: 1]
+
+  @spec mixing(list()) :: {:ok, non_neg_integer}
+  def mixing(types) do
+    inserted =
+      types
+      |> Enum.map(fn type -> {formation(type), list_entities(type)} end)
+      |> Enum.map(fn {formation, entries} -> entries |> Enum.map(&formation.format_and_save/1) end)
+      |> List.flatten()
+      |> Enum.count()
+
+    {:ok, inserted}
+  end
+
+  defp formation(type) do
+    case type do
+      :chef -> MarleySpoon.Formation.Chef
+      :recipe -> MarleySpoon.Formation.Recipe
+      :tag -> MarleySpoon.Formation.Tag
+    end
+  end
+end
